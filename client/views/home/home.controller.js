cd493f0bb9bb48f368a5d6610f5cac1c043ecef3
Van <vnguyen94@gmail.com>
2015-06-04 02:18:08 -0700

    word bank, css

--- client/views/home/home.controller.js
+++ client/views/home/home.controller.js
@@ -7,8 +7,26 @@ angular.module('121assignment3website')
 
     angular.extend(vm, {
       inputText: '',
-      query: '',
+      query: [],
       result: '',
+
+      wordBank: [
+        'mondego',
+        'machine learning',
+        'software engineering',
+        'security',
+        'student affairs',
+        'graduate courses',
+        'informatics',
+        'REST',
+        'computer games',
+        'information retrieval',
+      ],
+
+      wordBankQuery: function(word) {
+        vm.inputText = word;
+        vm.queryDatabase();
+      },
 
       queryDatabase: function() {
         document.getElementById('query-input').disabled = true;
@@ -20,11 +38,18 @@ angular.module('121assignment3website')
             document.getElementById('query-input').disabled = false;
       		})
       },
-      clearSearch: function() {
-      	vm.query = '';
-      	vm.result = '';
-      	vm.inputText = '';
+
+      clearSearch: function(index) {
+        vm.query.splice(index, 1);
+        if (vm.query.length !== 0) {
+          vm.inputText = vm.query.join(" ");
+          vm.queryDatabase();
+        }else {
+        	vm.result = '';
+        	vm.inputText = '';
+        }
       },
+
       openAboutModal: function() {
         $('#about-modal').foundation('reveal', 'open');
       }
