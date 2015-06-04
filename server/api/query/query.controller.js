3843bb3dd6ee9f5b6968d80510f2861f1f513af3
Van <vnguyen94@gmail.com>
2015-06-03 23:39:09 -0700

    blank urls get a default, ng-animate, updated text a bit

--- server/api/query/query.controller.js
+++ server/api/query/query.controller.js
@@ -1,31 +1,27 @@
 'use strict';
 
 var fs = require('fs');
-// var termidJSON = require('./query.data.json');
-// var tfidfJSON = require('./query.tfidf.json');
-// var docidJSON = require('./query.docid.json');
 
 function handleError (res, err) {
   return res.status(500).send(err);
 }
 
 exports.query = function (req, res) {
-
 	var key = req.params.query;
-
 
 	fs.readFile('server/api/query/query.key_termid.json', 'utf-8', function (err, result) {
     if (err) { return handleError(res, err); }
 
   	var termID = '' + JSON.parse(result)[key];
 
-
-
 		fs.readFile('server/api/query/query.termid_tfidf.json', 'utf-8', function (err, result) {
 	    if (err) { return handleError(res, err); }
 
 	    var toReturn = JSON.parse(result)[termID];
-	    console.log(toReturn);
+	    toReturn.forEach(function(row) {
+	    	row[1] = row[1] === '' ? '[UNTITLED PAGE]' : row[1];
+	    })
+
 			res.status(200).send(JSON.stringify(toReturn));
 		})
   })
